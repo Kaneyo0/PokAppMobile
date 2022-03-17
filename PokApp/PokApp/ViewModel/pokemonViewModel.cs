fa38f335955f7e24837202f5463c9107b7aa6d46
Léo <leofetter92@gmail.com>
2022-03-17 13:21:33 +0100

    duplication corrigée

--- PokApp/PokApp/ViewModel/pokemonViewModel.cs
+++ PokApp/PokApp/ViewModel/pokemonViewModel.cs
@@ -21,42 +21,45 @@ namespace PokApp.ViewModel
         {
             Pokemons = new ObservableCollection<Models.Pokemon>();
 
-            Device.BeginInvokeOnMainThread(async () =>
+            if (Application.Current.Properties.ContainsKey("FirstUse"))
             {
-
-                for (int i = 1; i <= 5; i++)
+                Device.BeginInvokeOnMainThread(async () =>
                 {
-                   
-                    var Types = "";
-                    var Abilities = new List<string>();
-                    var pokemonList = new List<Models.Pokemon>();
-                    var PokemonApi = await Task.Run(() => pokeClient.GetResourceAsync<Pokemon>(i));
-                    foreach (PokemonType Type in PokemonApi.Types)
+
+                    for (int i = 1; i <= 50; i++)
                     {
-                        Types += Type.Type.Name;
-                    }
+
+                        var Types = "";
+                        var Abilities = new List<string>();
+                        var pokemonList = new List<Models.Pokemon>();
+                        var PokemonApi = await Task.Run(() => pokeClient.GetResourceAsync<Pokemon>(i));
+                        foreach (PokemonType Type in PokemonApi.Types)
+                        {
+                            Types += Type.Type.Name;
+                        }
                         /*foreach (PokemonType Type in PokemonApi.Types)
                         {
                             Types.Add(Type.Type.Name);
                         }*/
                         foreach (PokemonAbility Ability in PokemonApi.Abilities)
-                    {
-                        Abilities.Add(Ability.Ability.Name);
+                        {
+                            Abilities.Add(Ability.Ability.Name);
+                        }
+                        await App.Database.SavePokemonAsync(new Models.Pokemon
+                        {
+                            Name = PokemonApi.Name,
+                            Picture = PokemonApi.Sprites.FrontDefault,
+                            Height = PokemonApi.Height,
+                            Weight = PokemonApi.Weight,
+                            Types = PokemonApi.Types[0].Type.Name,
+                            Abilities = Abilities,
+                            HP = PokemonApi.Stats[0].BaseStat,
+                            Atk = PokemonApi.Stats[1].BaseStat,
+                            Def = PokemonApi.Stats[2].BaseStat,
+                        });
                     }
-                    await App.Database.SavePokemonAsync(new Models.Pokemon
-                    {
-                        Name = PokemonApi.Name,
-                        Picture = PokemonApi.Sprites.FrontDefault,
-                        Height = PokemonApi.Height,
-                        Weight = PokemonApi.Weight,
-                        Types = PokemonApi.Types[0].Type.Name,
-                        Abilities = Abilities,
-                        HP = PokemonApi.Stats[0].BaseStat,
-                        Atk = PokemonApi.Stats[1].BaseStat,
-                        Def = PokemonApi.Stats[2].BaseStat,
-                    });
-                }
-            });
+                });
+            }
         }
     }
 }
